bf23990a65579aa5c37d7ea35fe3abcdd20a8759
AntonDanylenko <adanylenko@stuy.edu>
2018-10-18 14:09:21 -0400

    print_list works

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "linked_list.h"
-#include "linked_list.c"
 #include "library.h"
-#include "library.c"
 
 int main(){
+  struct song_node *temp;
+  temp = malloc(sizeof("artist name") + sizeof("song name") + sizeof(struct song_node *));
+  strcpy(temp->artist, "artist name");
+  strcpy(temp->name, "song name");
+  temp->next = NULL;
+
+  print_list(temp);
 }
